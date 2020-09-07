ee1f49bcbf07942a48aa69e29aacfc0efa01e85c
the 14th web <dennis.itua1@gmail.com>
2020-09-07 03:32:59 -0400

    updated route and server

--- server.js
+++ server.js
@@ -1,35 +1,40 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const app = express()
-const PORT = 5000
-const {MONGOURI} = require('./keys')
+const mongoose  = require('mongoose')
+const PORT = process.env.PORT || 5000
+const {MONGOURI} = require('./config/keys')
 
 
-
-
-mongoose.connect(MONGOURI, {
-    useNewUrlParser: true,
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
     useUnifiedTopology: true
-})
-mongoose.connection.on("connected", ()=>{
-    console.log("connected to db")
-})
 
-mongoose.connection.on("error", (err)=>{
-    console.log("err connecting to db", err)
+})
+mongoose.connection.on('connected',()=>{
+    console.log("conneted to mongo yeahh")
+})
+mongoose.connection.on('error',(err)=>{
+    console.log("err connecting",err)
 })
 
 require('./models/user')
 require('./models/post')
 
-// Always pass the middleware before the route
+//Always pass middleware before the route
 app.use(express.json())
 app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
+app.use(require('./routes/user'))
 
+
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 app.listen(PORT,()=>{
-    console.log("Server is running on ", PORT)
+    console.log("server is running on",PORT)
 })
-
-

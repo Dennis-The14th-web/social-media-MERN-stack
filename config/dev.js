543a69ee9454d06fb308babcf4375b4108321b94
the 14th web <dennis.itua1@gmail.com>
2020-09-07 22:37:31 -0400

    gitignore

--- config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-    MONGOLAB_TEAL_URI:"mongodb://realcoding:therealdeal1@ds151076.mlab.com:51076/heroku_dvjknbq9?authSource=heroku_dvjknbq9",
-    JWT_SECRET:"eggdeliciousxnotokmcmuffin"
-}

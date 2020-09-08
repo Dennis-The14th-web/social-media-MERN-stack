e95cc64b97b1225d6c02a0759ff9f37bebb8cd9f
the 14th web <dennis.itua1@gmail.com>
2020-09-07 21:05:25 -0400

    server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose  = require('mongoose')
 const PORT = process.env.PORT || 5000
-const {MONGOURI} = require('./config/keys')
+const { MONGOLAB_TEAL_URI } = require('./config/keys')
 
 
 mongoose.connect( MONGOLAB_TEAL_URI ,{
